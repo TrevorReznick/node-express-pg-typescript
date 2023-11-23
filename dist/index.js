f2a52eaff413ecf378802cf9d90b701a4fefc9b7
Tuo Nome <double.facessss@gmail.com>
2023-11-23 18:33:27 +0100

    testing router

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const index_1 = require("./app/routes/index");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -11,6 +12,8 @@ const port = process.env.PORT;
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
+/* @@ import Routes @@ */
+app.get('/test', index_1.test);
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import express, { Express, Request, Response } from 'express'
+import {test} from './app/routes/index'
+
 
 import dotenv from 'dotenv'
 
@@ -7,10 +9,19 @@ dotenv.config()
 const app: Express = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // Welcome Mesage
   res.send('Express + TypeScript Server')
 })
+
+/* @@ import Routes @@ */
+app.get('/test', test)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
 })
+
+
+
+
+
+
