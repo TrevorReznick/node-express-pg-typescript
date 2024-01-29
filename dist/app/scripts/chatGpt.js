9b3c016da6db53baad3e012001c50eea36769fc0
Tuo Nome <double.facessss@gmail.com>
2024-01-29 18:07:14 +0100

    fixing axios

--- dist/app/scripts/chatGpt.js
+++ dist/app/scripts/chatGpt.js
@@ -8,26 +8,29 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.doChat = void 0;
-const axios_1 = __importDefault(require("axios"));
 function doChat(msgObj) {
     return __awaiter(this, void 0, void 0, function* () {
         // Assicurati che msgObj contenga un array di oggetti con le proprietà 'role' e 'message'
-        const previousMessages = msgObj;
+        /*
+        const previousMessages: { role: string; message: string }[] = msgObj;
+    
         // Non è necessario utilizzare concat se previousMessages è già un array
-        let messages = previousMessages;
-        const prompt = messages.map((message) => `${message.role}: ${message.message}`).join('\n') + `\nAI:`;
-        const API_URL = 'https://api.openai.com/v1/completions';
+        let messages: { role: string; message: string }[] = previousMessages
+    
+        const prompt = messages.map((message) => `${message.role}: ${message.message}`).join('\n') + `\nAI:`
+        
+        const API_URL = 'https://api.openai.com/v1/completions'
+        
         const headers = {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
             Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        };
-        console.log('api key', process.env.OPENAI_API_KEY);
+        }
+    
+        console.log('api key', process.env.OPENAI_API_KEY)
+    
         const data = JSON.stringify({
             model: 'text-davinci-003',
             prompt: prompt,
@@ -37,19 +40,21 @@ function doChat(msgObj) {
             frequency_penalty: 0,
             presence_penalty: 0.6,
             stop: [' User:', ' AI:'],
-        });
+        })
+        
+       
         try {
-            const req = yield axios_1.default.post(API_URL, data, {
+            const req: any = await axios.post(API_URL, data, {
                 headers,
                 responseType: "json",
-            });
-            const res = yield req.json();
-            const result = res.choices[0];
-            return result; /*message: result.text*/
-        }
-        catch (error) {
-            throw error;
-        }
+            })
+            const res = await req.json()
+            const result = res.choices[0]
+            return result /*message: result.text*/
+        /*
+        } catch (error) {
+            throw error
+        }*/
     });
 }
 exports.doChat = doChat;
